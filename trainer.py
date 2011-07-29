8df4781b7aed8ec900d4b337bb090fcb59800725
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-29 17:40:43 +0100

    some more method skeletons, everything is still messy and broken

--- trainer.py
+++ trainer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
-import sys
+import sys, random
 from Tkinter import *
 
+global kanji
+global canvas
+
 def next():
-    print 'asdfasf'
+    r = random.randomint(0,len(kanji))
+    print r
+    display(canvas, r)
+    
+
+def display(canvas, index):
+    #c.create_text((10,10), text=kanji[index][0], font=ft, tags='kanji')
+    for h in kanji[index][1:]:
+        print h
 
 def main():
     """Runs when the file is run from the commandline
@@ -11,6 +22,8 @@ def main():
     ft = ('Meiryo', 40, 'normal')
     f = open(sys.argv[1])
     s = f.read().split('\n')
+    global kanji
+    global canvas
     kanji = []
     for k_set in s:
         kanji.append(k_set.split(' '))
@@ -20,7 +33,8 @@ def main():
     nxt.pack()
     c = Canvas(m)
     c.pack()
-    c.create_text((10,10), text=kanji[0][0], anchor='nw', font=ft)
+    canvas = c
+    c.create_text((10,10), text=kanji[0][0], font=ft, tags='kanji')
     c.mainloop()
     
 if __name__ == '__main__':
