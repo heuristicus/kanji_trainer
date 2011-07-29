fc932e6550a61e0e7ea9b84dffe20387ed77f61b
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-29 18:34:31 +0100

    switch to class based structure, even though it isnt really necessary

--- trainer.py
+++ trainer.py
@@ -5,58 +5,54 @@ from Tkinter import *
 global kanji
 global canvas
 
-def next():
-    r = random.randint(0,len(kanji) - 1)
-    display(canvas, r)
-    
-def display(canvas, index):
-    big = ('Meiryo', 90, 'normal')
-    small = ('Meiryo', 18, 'normal')
-    k = kanji[index][0]
-    disp = ''
-    for h in kanji[index][1:]:
-        disp += '%s\n'%(h)
-    canvas.delete('all')
-    canvas.create_text((int(canvas.cget('width'))/2,int(canvas.cget('height'))/4), text=k, font=big, tags='kanji')
-    canvas.create_text((int(canvas.cget('width'))/2,2*int(canvas.cget('height'))/3), text=disp, font=small, tags='hiragana')
+class KanjiTrainer:
 
-def main():
-    """Runs when the file is run from the commandline
-    """
+    def __init__(self):
+        f = open(sys.argv[1])
+        s = f.read().split('\n')[:-1] # last line is empty
+        global kanji
+        global canvas
+        kanji = []
+        for k_set in s:
+            kanji.append(k_set.split(' '))
     
-    f = open(sys.argv[1])
-    s = f.read().split('\n')[:-1] # last line is empty
-    global kanji
-    global canvas
-    kanji = []
-    for k_set in s:
-        kanji.append(k_set.split(' '))
+        m = Tk()
+        m.title('Kanji Trainer')
+        
+        wh = m.winfo_screenheight()
+        ww = m.winfo_screenwidth()
+        
+        w = 400
+        h = 400
+        x = (ww/2) - (w/2)
+        y = (wh/2) - (h/2)
+        print x, y
+        
+        m.geometry("%dx%d+%d+%d"%(w,h,x,y))
     
-    m = Tk()
-    m.title('Kanji Trainer')
+    
+        nxt = Button(m, text='Next', command=self.next)
+        nxt.pack()
+        c = Canvas(m, height=400, width=400)
+        c.pack()
+        
+        canvas = c
+        c.mainloop()
 
-    wh = m.winfo_screenheight()
-    ww = m.winfo_screenwidth()
+    def next(self):
+        r = random.randint(0,len(kanji) - 1)
+        self.display(canvas, r)
     
-    w = 400
-    h = 400
-    x = (ww/2) - (w/2)
-    y = (wh/2) - (h/2)
-    print x, y
-    
-    m.geometry("%dx%d+%d+%d"%(w,h,x,y))
-    
-    
-    nxt = Button(m, text='Next', command=next)
-    nxt.pack()
-    c = Canvas(m, height=400, width=400)
-    c.pack()
-    
-    canvas = c
-    c.mainloop()
+    def display(self, canvas, index):
+        big = ('Meiryo', 90, 'normal')
+        small = ('Meiryo', 18, 'normal')
+        k = kanji[index][0]
+        disp = ''
+        for h in kanji[index][1:]:
+            disp += '%s\n'%(h)
+            canvas.delete('all')
+            canvas.create_text((int(canvas.cget('width'))/2,int(canvas.cget('height'))/4), text=k, font=big, tags='kanji')
+            canvas.create_text((int(canvas.cget('width'))/2,2*int(canvas.cget('height'))/3), text=disp, font=small, tags='hiragana')
 
-    
 if __name__ == '__main__':
-
-    main()
-    
+    KanjiTrainer()
