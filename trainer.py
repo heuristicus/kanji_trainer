241b07ae24c5a51485934a8085995414dc9aee3e
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-29 19:26:45 +0100

    cleaned up a little bit

--- trainer.py
+++ trainer.py
@@ -7,37 +7,29 @@ global canvas
 
 class KanjiTrainer:
 
-    def __init__(self, klist):
-        
-    
-        m = Tk()
-        m.title('Kanji Trainer')
-        
-        wh = m.winfo_screenheight()
-        ww = m.winfo_screenwidth()
-        
-        w = 400
-        h = 400
-        x = (ww/2) - (w/2)
-        y = (wh/2) - (h/2)
-        print x, y
-        
-        m.geometry("%dx%d+%d+%d"%(w,h,x,y))
-    
-    
-        nxt = Button(m, text='Next', command=self.next)
+    def __init__(self, klist):    
+        self.root = Tk()
+        self.root.title('Kanji Trainer')
+        nxt = Button(self.root, text='Next', command=self.next)
         nxt.pack()
-        shw = Button(m, text='Reveal', command=self.reveal)
+        shw = Button(self.root, text='Reveal', command=self.reveal)
         shw.pack()
-        c = Canvas(m, height=400, width=400)
-        c.pack()
-        
-        self.canvas = c
-        c.mainloop()
+        self.canvas = Canvas(self.root, height=400, width=400)
+        self.canvas.pack()
+        self.centre_window()
+        self.canvas.mainloop()
 
+    def centre_window(self):
+        window_height = self.root.winfo_screenheight()
+        window_width = self.root.winfo_screenwidth()
+        h = int(self.canvas.cget('height'))
+        w = int(self.canvas.cget('width'))
+        x = (window_width/2) - (w/2)
+        y = (window_height/2) - (h/2)
+        self.root.geometry("%dx%d+%d+%d"%(w,h,x,y))
+    
     def next(self):
-        r = random.randint(0,len(kanji) - 1)
-        self.display(self.canvas, r)
+        self.display(self.canvas, random.randint(0,len(kanji) - 1))
 
     def reveal(self):
         self.canvas.itemconfigure('hiragana', state='normal')
@@ -45,19 +37,19 @@ class KanjiTrainer:
     def display(self, canvas, index):
         big = ('Meiryo', 90, 'normal')
         small = ('Meiryo', 18, 'normal')
+        cheight = int(canvas.cget('height'))
+        cwidth = int(canvas.cget('width'))
         k = kanji[index][0]
         disp = ''
         for h in kanji[index][1:]:
             disp += '%s\n'%(h)
             canvas.delete('all')
-            canvas.create_text((int(canvas.cget('width'))/2,int(canvas.cget('height'))/4), text=k, font=big, tags='kanji')
-            canvas.create_text((int(canvas.cget('width'))/2,2*int(canvas.cget('height'))/3), text=disp, font=small, tags='hiragana', state='hidden')
+            canvas.create_text(cwidth/2, cheight/4, text=k, font=big, tags='kanji')
+            canvas.create_text(cwidth/2, 2*cheight/3, text=disp, font=small, tags='hiragana', state='hidden')
 
 if __name__ == '__main__':
-    f = open(sys.argv[1])
-    s = f.read().split('\n')[:-1] # last line is empty
+    s = open(sys.argv[1]).read().split('\n')[:-1] # last line is empty
     kanji = []
     for k_set in s:
         kanji.append(k_set.split(' '))
-            
     KanjiTrainer(kanji)
