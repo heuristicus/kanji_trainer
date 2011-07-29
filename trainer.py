72acf55f8abefa5f9806f28bf2548613ce0b0947
Michal Staniaszek <m.staniaszek@gmail.com>
2011-07-29 17:36:43 +0100

    super basic test stuff - seems like this should be easy

--- trainer.py
+++ trainer.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
-import sys, Tkinter
+import sys
+from Tkinter import *
 
-def ask_question():
-    """Asks a question of the user about a specific kanji
-    """
-    print 'bb'
-
+def next():
+    print 'asdfasf'
 
 def main():
     """Runs when the file is run from the commandline
     """
+    ft = ('Meiryo', 40, 'normal')
     f = open(sys.argv[1])
     s = f.read().split('\n')
-    print s
+    kanji = []
+    for k_set in s:
+        kanji.append(k_set.split(' '))
     
-    
+    m = Tk()
+    nxt = Button(m, text='Next', command=next)
+    nxt.pack()
+    c = Canvas(m)
+    c.pack()
+    c.create_text((10,10), text=kanji[0][0], anchor='nw', font=ft)
+    c.mainloop()
     
 if __name__ == '__main__':
 
